5e69f8daf020d387775bff98088f356db25413d0
franceska <franceska.rrokajh@epitech.eu>
2024-09-19 15:21:45 +0200

    [ADD] poc/frontend, react

---
+++ www/react-poc/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './components/TodoList';
+// import LoginForm from './components/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
