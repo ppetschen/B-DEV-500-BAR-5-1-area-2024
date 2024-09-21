f68f3ec2781083ca47f356e62776b8ab35926d67
Javier Ríos <javier.rios-urbano@epitech.eu>
2024-09-21 19:46:05 +0200

    fix: python route payloads (#26)

--- demo/server/python/todo_app/__main__.py
+++ demo/server/python/todo_app/__main__.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "todo_app.main:app",
         host="127.0.0.1",
-        port=8000,
+        port=3000,
         reload=True,
     )
--- demo/server/python/todo_app/routers/todos.py
+++ demo/server/python/todo_app/routers/todos.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict, Union
 
 from bson import ObjectId
 from fastapi import APIRouter, HTTPException, status
@@ -14,32 +14,39 @@ router = APIRouter(
 )
 
 
+# Helper function to format response
+def format_response(data: Union[List, Dict]):
+    return {"data": data}
+
+
 # Retrieve all todo objects [GET] /todo
-@router.get("/", response_model=List[TodoItem])
+@router.get("/", response_model=Dict[str, List[TodoItem]])
 async def list_todo_items():
     todos = await db["todos"].find().to_list(1000)
     for todo in todos:
         todo["id"] = str(todo["_id"])
-    return todos
+    return format_response(todos)
 
 
 # Retrieve one todo object based on the provided ID [GET] /todo/{id}
-@router.get("/{id}", response_model=TodoItem)
+@router.get("/{id}", response_model=Dict[str, TodoItem])
 async def get_todo_item(id: str):
     if (todo := await db["todos"].find_one({"_id": ObjectId(id)})) is not None:
         todo["id"] = str(todo["_id"])
-        return todo
+        return format_response(todo)
     raise HTTPException(status_code=404, detail="Todo item not found")
 
 
 # Creates a new todo object [POST] /todo
-@router.post("/", response_model=TodoItem, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/", response_model=Dict[str, TodoItem], status_code=status.HTTP_201_CREATED
+)
 async def create_todo_item(todo: TodoItemSchema):
     new_todo = await db["todos"].insert_one(todo.model_dump())
     created_todo = await db["todos"].find_one({"_id": new_todo.inserted_id})
     if created_todo:
         created_todo["id"] = str(created_todo["_id"])
-    return created_todo
+    return format_response(created_todo)
 
 
 # Permanently deletes the todo object specified by its ID [DELETE] /todo/{id}
@@ -52,7 +59,7 @@ async def delete_todo_item(id: str):
 
 
 # Updates the entire object, replacing its current state [PUT] /todo/{id}
-@router.put("/{id}", response_model=TodoItem)
+@router.put("/{id}", response_model=Dict[str, TodoItem])
 async def update_todo_item(id: str, todo: TodoItemSchema):
     update_result = await db["todos"].replace_one({"_id": ObjectId(id)}, todo.dict())
 
@@ -60,13 +67,13 @@ async def update_todo_item(id: str, todo: TodoItemSchema):
         updated_todo = await db["todos"].find_one({"_id": ObjectId(id)})
         if updated_todo:
             updated_todo["id"] = str(updated_todo["_id"])
-        return updated_todo
+        return format_response(updated_todo)
 
     raise HTTPException(status_code=404, detail="Todo item not found")
 
 
 # Updates an object partially, modifying only fields specified in the body [PATCH] /todo/{id}
-@router.patch("/{id}", response_model=TodoItem)
+@router.patch("/{id}", response_model=Dict[str, TodoItem])
 async def patch_todo_item(id: str, todo: UpdateTodoItemSchema):
     update_result = await db["todos"].update_one(
         {"_id": ObjectId(id)}, {"$set": todo.model_dump(exclude_unset=True)}
@@ -76,6 +83,6 @@ async def patch_todo_item(id: str, todo: UpdateTodoItemSchema):
         updated_todo = await db["todos"].find_one({"_id": ObjectId(id)})
         if updated_todo:
             updated_todo["id"] = str(updated_todo["_id"])
-        return updated_todo
+        return format_response(updated_todo)
 
     raise HTTPException(status_code=404, detail="Todo item not found")
