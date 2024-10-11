cd607521c0d4f2484f98b2fa0dae434ff40e6d21
timpaepitech <tim1.lundstedt@epitech.eu>
2024-10-12 00:41:28 +0200

    Revert "feat: discord service added to service management (#67)" (#68)
    This reverts commit 740571989541358d57a3271ec63c4976529275a9.

--- server/service-management/sources/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "bun" {
-  interface Env {
-    PORT: number;
-    GATEWAY_HOST: string;
-    KONG_DELAY_MS: number | undefined;
-  }
-}
--- server/user-management/utils.ts
+++
@@ -1,6 +0,0 @@
-const HOSTS = {
-  DATABASE: process.env["DATABASE_HOST"],
-};
-
-export const host = (key: keyof typeof HOSTS, path: string) =>
-  `http://${HOSTS[key]}${path}`;
