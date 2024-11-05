d42f724b331f51a59147be7f8031f235b15c400d
Javier R <javier.rios-urbano@epitech.eu>
2024-11-05 04:30:46 +0100

    add: implement `completions` route scaffold integration

--- server/area-composition/sources/utils.ts
+++ server/area-composition/sources/utils.ts
@@ -1,4 +1,8 @@
 import process from "node:process";
+import { Octokit } from "@octokit/rest";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v10";
+
 const HOSTS = {
   DATABASE: process.env["DATABASE_HOST"],
   ACTION: process.env["ACTION_HOST"],
@@ -7,3 +11,31 @@ const HOSTS = {
 
 export const host = (key: keyof typeof HOSTS, path: string) =>
   `http://${HOSTS[key]}${path}`;
+
+const getGitHubCompletions = async (
+  accessToken: string,
+): Promise<Record<string, unknown>> => {
+  const octokit = new Octokit({ auth: accessToken });
+  const { data } = await octokit.repos.listForAuthenticatedUser();
+
+  return { data: data.map(({ owner, name }) => ({ owner, name })) };
+};
+
+const getDiscordCompletions = async (
+  accessToken: string,
+): Promise<Record<string, unknown>> => {
+  const rest = new REST({ version: "10" }).setToken(accessToken);
+  const channels = await rest.get(Routes.userChannels());
+
+  return { data: channels };
+};
+
+const completionsMap = {
+  "GITHUB": getGitHubCompletions,
+  "DISCORD": getDiscordCompletions,
+} as const;
+
+export const complete = async (
+  service: keyof typeof completionsMap,
+  accessToken: string,
+) => completionsMap[service](accessToken);
