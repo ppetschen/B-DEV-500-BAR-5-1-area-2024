e27eeae4bb3d5dfd1c0b2fbcc91edd50e29268b2
Javier R <javier.rios-urbano@epitech.eu>
2024-11-05 19:43:03 +0100

    fix: use octokit `pagination`, use correct `completion` endpoint

--- server/area-composition/sources/utils.ts
+++ server/area-composition/sources/utils.ts
@@ -1,7 +1,5 @@
 import process from "node:process";
 import { Octokit } from "@octokit/rest";
-import { REST } from "@discordjs/rest";
-import { Routes } from "discord-api-types/v10";
 
 const HOSTS = {
   DATABASE: process.env["DATABASE_HOST"],
@@ -16,23 +14,35 @@ const getGitHubCompletions = async (
   accessToken: string,
 ): Promise<Record<string, unknown>> => {
   const octokit = new Octokit({ auth: accessToken });
-  const { data } = await octokit.repos.listForAuthenticatedUser();
+  const data = await octokit.paginate(octokit.repos.listForAuthenticatedUser, {
+    per_page: 100,
+  });
 
-  return { data: data.map(({ owner, name }) => ({ owner, name })) };
+  return {
+    data: data.map(({ owner, name }) => ({ owner: owner.login, name })),
+  };
 };
 
 const getDiscordCompletions = async (
   accessToken: string,
 ): Promise<Record<string, unknown>> => {
-  const rest = new REST({ version: "10" }).setToken(accessToken);
-  const channels = await rest.get(Routes.userChannels());
+  const baseUrl = "https://discord.com/api/v10";
+  const headers = {
+    "Authorization": `Bearer ${accessToken}`,
+    "Content-Type": "application/json",
+  };
 
-  return { data: channels };
+  const guildsResponse = await fetch(`${baseUrl}/users/@me/guilds`, {
+    headers,
+  });
+  const guilds = await guildsResponse.json();
+
+  return { data: guilds.map(({ id }: { id: string }) => ({ guildId: id })) };
 };
 
 const completionsMap = {
-  "GITHUB": getGitHubCompletions,
-  "DISCORD": getDiscordCompletions,
+  "github": getGitHubCompletions,
+  "discord": getDiscordCompletions,
 } as const;
 
 export const complete = async (
