375089c670745cf8a84cfaa343ea4584c15730a7
Javier R <javier.rios-urbano@epitech.eu>
2024-11-07 12:32:02 +0100

    add: `google` email scaffold

--- server/reaction/sources/utils.ts
+++ server/reaction/sources/utils.ts
@@ -1,5 +1,5 @@
 import process from "node:process";
-import type { InternalConfig } from "./types";
+import type { HookContext, InternalConfig } from "./types";
 import ejs from "ejs";
 
 const HOSTS = {
@@ -61,36 +61,71 @@ export const renderEjs = async (markup: string, context: unknown) => {
   return ejs.render(markup, context);
 };
 
-const sendDiscordWebhook = async (content: string, url: string) => {
-  const response = await fetch(url, {
+const sendDiscordWebhook = async (
+  { execution_endpoint, view }: HookContext,
+) => {
+  const response = await fetch(execution_endpoint, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ content }),
+    body: JSON.stringify({ content: view }),
   });
 
   if (!response.ok) {
     throw new Error("Failed to send webhook");
   }
 };
-/**
- * Since this is not a webhook, we dont need the createWebHook function? 
- * Also is there a need to put this in either the createWebHookMap or sendWebHookMap?
- */
-const sendGoogleMail = async (content: string, url: string) => {
+
+const sendGoogleMail = async (
+  { reaction_id, view }: HookContext,
+) => {
   // Here i would need a TO, SUBJECT and a BODY for the email
   // I would also need the access_token to this function.
   // Everything else will be the same for all emails sent
   // Can you guide me on how to implement this for this architecture
+  const findRequest = await fetch(host("DATABASE", "/reaction/find"), {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ id: reaction_id }),
+  });
+
+  if (!findRequest.ok) {
+    throw new Error("Failed to find reaction");
+  }
+
+  const { owner_id } = await findRequest.json();
+
+  const userRequest = await fetch(
+    host("DATABASE", "/user-management/get-user-by-id"),
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ consumer: owner_id }),
+    },
+  );
+
+  if (!userRequest.ok) {
+    throw new Error("Failed to find user");
+  }
+
+  const { email } = await userRequest.json();
+
+  const _emailContext = {
+    to: email,
+    subject: `New Notification from ${reaction_id}`,
+    body: view,
+  };
+
+  // TODO(tim): Implement sending email
 };
 
 const sendWebHookMap = {
   "discord": sendDiscordWebhook,
+  "google-mail": sendGoogleMail,
 } as const;
 
 export const send = async (
   type: keyof typeof sendWebHookMap,
-  content: string,
-  url: string,
-): Promise<void> => sendWebHookMap[type](content, url);
+  context: HookContext,
+): Promise<void> => sendWebHookMap[type](context);
